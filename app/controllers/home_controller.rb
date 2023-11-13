b2dc855b1d9064a34527fde8e6157c6fb7e4b62c
Saurabh Lodha <saurabhlodha11@gmail.com>
2023-11-13 17:32:31 +0000

    Adds WIP page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def wip
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'home/wip'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "home#wip"
 end
